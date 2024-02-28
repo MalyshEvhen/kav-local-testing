5a3e2d327ae047a0884a46a0cfc9aeb7e4490567
Evhen <malysh.evgeniy@gmail.com>
2024-02-28 14:48:06 +0200

    add dockerfiles

--- runner_back.bat
+++ runner_back.bat
@@ -6,6 +6,14 @@ git clone -b dev https://github.com/web-synergy/museum-back.git
 git clone -b main https://github.com/web-synergy/museum-main.git
 git clone -b main https://github.com/web-synergy/museum-admin.git
 
+:: Copy Dockerfile to `museum-main` and `museum-admin` dirs
+xcopy .\Dockerfile .\museum-main\
+xcopy .\Dockerfile .\museum-admin\
+
+:: Copy `docker` dir recursevly to `museum-main` and `museum-admin` dirs
+xcopy /s \docker \museum-main\
+xcopy /s \docker \museum-admin\
+
 :: Run a docker compose up command with --build flag
 docker compose up --build
-
+pause
