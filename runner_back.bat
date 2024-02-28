e6418422690b6ce8fa9fe6925c1a11cc8cf7a5c1
Evhen <malysh.evgeniy@gmail.com>
2024-02-28 14:02:35 +0200

    add web services

--- runner_back.bat
+++ runner_back.bat
@@ -3,11 +3,9 @@ setlocal enabledelayedexpansion
 
 :: Clone repository
 git clone -b dev https://github.com/web-synergy/museum-back.git
+git clone -b dev https://github.com/web-synergy/museum-main.git
+git clone -b dev https://github.com/web-synergy/museum-admin.git
 
 :: Run a docker compose up command with --build flag
-docker compose --file docker-compose-backend.yml up --build
+docker compose up --build
 
-docker compose --file docker-compose-backend.yml down
-
-:: Removing the cloned repository after running the command
-rmdir /s /q museum-back
