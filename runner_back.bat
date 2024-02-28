b82c74941632547c6129fa4987602bcabedd0ad9
Evhen <malysh.evgeniy@gmail.com>
2024-02-28 14:34:09 +0200

    fix: change branches

--- runner_back.bat
+++ runner_back.bat
@@ -3,8 +3,8 @@ setlocal enabledelayedexpansion
 
 :: Clone repository
 git clone -b dev https://github.com/web-synergy/museum-back.git
-git clone -b dev https://github.com/web-synergy/museum-main.git
-git clone -b dev https://github.com/web-synergy/museum-admin.git
+git clone -b main https://github.com/web-synergy/museum-main.git
+git clone -b main https://github.com/web-synergy/museum-admin.git
 
 :: Run a docker compose up command with --build flag
 docker compose up --build
